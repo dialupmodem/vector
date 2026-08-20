5e4ac05a53f727611f2c549b3eedf48c4635ca13
dialupmodem <18317934+dialupmodem@users.noreply.github.com>
2026-08-20 08:51:47 -0400

    remove default files, set default route to applications index

--- Vector/Program.cs
+++ Vector/Program.cs
@@ -30,6 +30,6 @@ app.MapRazorPages()
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Applications}/{action=Index}/{id?}");
 
 app.Run();
